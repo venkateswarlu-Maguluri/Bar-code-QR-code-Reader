ae0f6bd808b3fe5c52e51186c23d4cfc00931f9d
venkateswarlu-Maguluri <85087547+venkateswarlu-Maguluri@users.noreply.github.com>
2025-08-06 16:16:53 +0530

    Add files via upload

---
+++ Bar code And QR code Reader.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+pip install pyzbar
+
+
+# In[ ]:
+
+
+
+
+
+# In[2]:
+
+
+pip install opencv-python
+
+
+# In[3]:
+
+
+import cv2
+from pyzbar import pyzbar
+
+
+# In[4]:
+
+
+def read_barcodes(frame):
+    barcodes = pyzbar.decode(frame)
+    for barcode in barcodes:
+        x, y , w, h = barcode.rect
+        #1
+        barcode_info = barcode.data.decode('utf-8')
+        cv2.rectangle(frame, (x, y),(x+w, y+h), (0, 255, 0), 2)
+        
+        #2
+        font = cv2.FONT_HERSHEY_DUPLEX
+        cv2.putText(frame, barcode_info, (x + 6, y - 6), font, 2.0,(225,0,0), 1)
+        #3
+        with open("barcode_result.txt", mode ='w') as file:
+            file.write("Recognized Barcode:" + barcode_info)
+    return frame
+
+
+# In[ ]:
+
+
+def main():
+    #1
+    camera = cv2.VideoCapture(0)
+    ret, frame = camera.read()
+    #2
+    while ret:
+        ret, frame = camera.read()
+        frame = read_barcodes(frame)
+        cv2.imshow('Barcode/QR code reader', frame)
+        if cv2.waitKey(1) & 0xFF == 27:
+            break
+    #3
+    camera.release()
+    cv2.destroyAllWindows()
+#4
+if __name__ == '__main__':
+    main()
+
+
+# In[ ]:
+
+
+
+
